ae8633c4af083baf2de3eb278d87b635edcff8b0
Puning <wangpuning28@gmail.com>
2024-05-30 14:22:40 +1000

    create _init_.py

---
+++ src/_init_.py
@@ -0,0 +1,10 @@
+"""my doctsring
+"""
+__author__ = "PUNING WANG"
+__copyright__ = "Copyright 2014-2025, Puning Wang"
+__credits__ = ["PUNING WANG"]
+__license__ = "BSD"
+__version__ = "2024.5.21"  # A DATE BASED VERSION
+__maintainer__ = "PUNING WANG"
+__email__ = "wangpuning28@gmail.com"
+__status__ = "alpha"
