034400fbcbee3d6403ec52e091a09e2d78d77df4
Puning <wangpuning28@gmail.com>
2024-08-09 11:00:31 +1000

    script for calculate ens, non-stationarity entropy... difference magnitude quantification

--- src/clock_project/simulation/magnitude_quantification.py
+++ src/clock_project/simulation/magnitude_quantification.py
@@ -4,7 +4,7 @@ from scipy.linalg import expm
 import numpy as np
 
 
-
+unifor_distribution = [0.25, 0.25, 0.25, 0.25]
 
 def calculate_ENS(p0, Q, t):
     return expected_number_subs(p0, Q, t)
@@ -13,3 +13,17 @@ def calculate_non_stationarity(p0, Q, t):
     pi_deriv = dot(p0, dot(Q, expm(Q * t)))
     conv = np.linalg.norm(pi_deriv)
     return conv
+
+def entropy_calculation(pk):
+    return -np.sum(pk*np.log2(pk))
+
+def calculate_information(p0):
+    return entropy_calculation(unifor_distribution) - entropy_calculation(p0)
+
+
+def random_nucleotide_distribution():
+    # Generate 4 random numbers
+    random_numbers = np.random.rand(4)
+    # Normalize these numbers so that their sum equals 1
+    distribution = random_numbers / random_numbers.sum()
+    return distribution
