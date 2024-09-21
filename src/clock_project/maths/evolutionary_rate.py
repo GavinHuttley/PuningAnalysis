79133768f3e02e1e63e8f335f88dabf56196d68d
Puning <wangpuning28@gmail.com>
2024-09-21 12:22:52 +1000

    add ENS calculation for one process

--- src/clock_project/maths/evolutionary_rate.py
+++ src/clock_project/maths/evolutionary_rate.py
@@ -120,3 +120,32 @@ def calculate_non_stationary_mu_prime(Q, pi_0, t):
     mu_prime_t = -pi_0.dot(Q).dot(expm(Q * t)).dot(np.diagonal(Q))
     
     return mu_prime_t
+
+#stationary process ENS accumulation function
+def generate_ENS(Q1, Q2, t_range, t1):
+    """
+    Generates the ENS over a range of time points using two different Q matrices before and after a specified time point t1.
+    
+    Parameters:
+    - pi: A numpy array of shape (1, 4) representing the vector pi.
+    - Q1, Q2: Two numpy arrays of shape (4, 4) representing the original and new rate matrices.
+    - t_range: numpy.linspace defining the start and end of the time range.
+    - t1: The time point at which to switch from using Q1 to Q2.
+    
+    Returns:
+    - A list of ENS values for each time point in the range.
+    """
+    pi = calculate_stationary_distribution(Q1)
+
+    ens_values = []
+    ens_accumulated = 0  # To keep track of the accumulated ENS value
+    
+    for t in t_range:
+        if t <= t1:
+            ens = -np.sum(pi * np.diag(Q1)) * t + ens_accumulated
+            ens_values.append(ens)
+        else:
+            ens_2 = -np.sum(pi * np.diag(Q2)) * (t-t1) + ens # Update the accumulated ENS at t1 to continue from this point using Q2
+            ens_values.append(ens_2)
+    
+    return ens_values
