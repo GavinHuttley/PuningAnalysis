e50808495027c5769294b13271bc2ac8353098c0
Puning <wangpuning28@gmail.com>
2025-08-08 00:55:55 +1000

    update directories

--- nbks/plot_utils/project_paths.py
+++ nbks/plot_utils/project_paths.py
@@ -5,7 +5,7 @@ from plotly.io import write_image
 
 ROOT_DIR = pathlib.Path(__file__).parent.parent.parent
 
-OUTPUT_ROOT = ROOT_DIR / "MutationDiseqMS"
+OUTPUT_ROOT = ROOT_DIR / "outputs"
 
 FIG_DIR = OUTPUT_ROOT / "figs"
 FIG_DIR.mkdir(parents=True, exist_ok=True)
@@ -13,11 +13,12 @@ FIG_DIR.mkdir(parents=True, exist_ok=True)
 TABLE_DIR = OUTPUT_ROOT / "tables"
 TABLE_DIR.mkdir(parents=True, exist_ok=True)
 
-SUPP_TABLE_DIR = OUTPUT_ROOT / "tables_supp"
-SUPP_TABLE_DIR.mkdir(parents=True, exist_ok=True)
+# SUPP_TABLE_DIR = OUTPUT_ROOT / "tables_supp"
+# SUPP_TABLE_DIR.mkdir(parents=True, exist_ok=True)
 
-DATA_DIR = ROOT_DIR / "MutationDiseqAnalysis/data"
-RESULT_DIR = ROOT_DIR / "MutationDiseqAnalysis/results"
+DATA_DIR = "/Users/gulugulu/clock/mammal_orthologs_hsap_1"
+# RESULT_DIR = ROOT_DIR / "MutationDiseqAnalysis/results"
+
 
 class pdf_writer:
     """class that handles super annoying mathjax warning box in plotly pdf's"""
